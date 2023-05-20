ad3eb7b2cbdfaafc736b9c3826ce71759b555c94
Adithyan <sadithyan403@gmail.com>
2023-05-20 10:54:37 +0530

    GET request,action,retrivieng data

--- sample server in if.js
+++ sample server in if.js
@@ -1,18 +1,22 @@
 var http=require("http")
 var readhtml=require("fs")
+var url=require("url")
 
 
 
 
 http.createServer(function (request,response){
-    if(request.url==="/"){
+    var q=url.parse(request.url,true)   // this true is passed to get the data as an object
+    
+
+    if(q.pathname==="/welcome"){
         readhtml.readFile("html.html",function(err,data){
             response.writeHead(200,{"Content-Type":"text/html"})
             response.write(data)
             response.end()
         }) 
     }
-    else if(request.url==="/login"){
+    else if(q.pathname==="/login"){
         readhtml.readFile("login page.html",function(err,data){
             response.writeHead(200,{"Content-Type":"text/html"})
             response.write(data)
@@ -20,6 +24,14 @@ http.createServer(function (request,response){
         })
         
         
+    }
+    else if(q.pathname==="/loginaction"){
+        response.write("login successfull")
+        response.write(q.query.username)
+        console.log("login successfull")
+        console.log(q.query)
+        response.end()
+
     }
     else{
         response.writeHead(404,{"Content-Type":"text/html"})
